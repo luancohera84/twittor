e0c6126ea264ae1c2cc993ddaaaa17e34a988ee2
Luis Cortes <lcortes@intelibpo.com>
2023-05-20 01:03:55 -0500

    modificaciones para ios

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 importScripts('js/sw-utils.js');
 
 // Declaracion de constantes
-const STATIC_CACHE = 'static-v6';
+const STATIC_CACHE = 'static-v1';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
