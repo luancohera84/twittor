5fab6259fc0194e28faa465e92b82f3f90f898cf
Luis Cortes <lcortes@intelibpo.com>
2023-05-20 01:11:50 -0500

    modificaciones de iphone x+

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@
 importScripts('js/sw-utils.js');
 
 // Declaracion de constantes
-const STATIC_CACHE = 'static-v1';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const STATIC_CACHE = 'static-v2';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
@@ -49,6 +49,11 @@ self.addEventListener( 'activate' , event => {
             if (  key !== STATIC_CACHE && key.includes('static') ) {
                 return caches.delete(key);
             }
+
+            if (  key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
+                return caches.delete(key);
+            }
+
 
         });
 
